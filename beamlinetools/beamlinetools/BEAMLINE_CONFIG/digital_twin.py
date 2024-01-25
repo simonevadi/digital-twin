5a39b0c031913332719e795284fc5d5b03afedc0
simone.vadilonga1 <simone.vadilonga@gmail.com>
2024-01-25 17:45:11 +0100

    add server example, commented

--- beamlinetools/beamlinetools/BEAMLINE_CONFIG/digital_twin.py
+++ beamlinetools/beamlinetools/BEAMLINE_CONFIG/digital_twin.py
@@ -14,4 +14,22 @@ rml_file_name = 'elisa.rml'
 rml_path = os.path.join(script_dir, 'rml', rml_file_name)
 # insert here the path to the rml file that you want to use
 
+
+# local
 RaypyngOphydDevices(RE=RE, rml_path=rml_path, temporary_folder=None, name_space=None, prefix=None, ray_ui_location=None)
+
+
+# with server
+# from raypyng_bluesky.RaypyngOphydDevices import RaypyngOphydDevices
+
+# RaypyngOphydDevices(RE=RE, # this is the RunEngine
+#                     rml_path=rml_path, # path to elisa.rml
+#                     prefix='rp', 
+#                     temporary_folder=None, 
+#                     name_space=None, 
+#                     simulation_engine='rayuiClient', 
+#                     ip='localhost',
+#                     port=12345)
+
+# to run the server use file 
+# python test/test_asyncio_server.py
